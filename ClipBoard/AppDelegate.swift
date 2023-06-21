2d56ea153360fcd4f797f94671c36f39d4140558
buldubu <buldubu@gmail.com>
2023-06-22 00:40:33 +0300

    Initial Commit

---
+++ ClipBoard/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ClipBoard
+//
+//  Created by Burak Buldu on 22.06.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ ClipBoard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClipBoard
+//
+//  Created by Burak Buldu on 22.06.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
